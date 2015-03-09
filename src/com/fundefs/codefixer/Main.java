a6f15758ca4b97d6973c5696c1fe125ca078d65e
ARedT <fundefs@gmail.com>
2015-03-09 23:51:00 +0100

    Update

--- src/com/fundefs/codefixer/Main.java
+++ src/com/fundefs/codefixer/Main.java
@@ -1,5 +1,7 @@
 package com.fundefs.codefixer;
 
+import com.fundefs.codefixer.update.UpdateManager;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,7 +11,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	
-	public static final double version = 0.0;
+	public static final String version = "0.1.0.0";
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -17,7 +19,7 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent root = (Parent) FXMLLoader.load(getClass().getResource("/res/Layout.fxml"));
+		Parent root = (Parent) FXMLLoader.load(getClass().getResource(UpdateManager.versionIsNewest() ? "/res/Layout.fxml" : "/res/LayoutUpdate.fxml"));
 		stage.setTitle("CodeFixer by Fundefs \u00A9");
 		stage.setResizable(false);
 		stage.setScene(new Scene(root));
--- src/com/fundefs/codefixer/update/UpdateManager.java
+++ src/com/fundefs/codefixer/update/UpdateManager.java
@@ -3,7 +3,7 @@ package com.fundefs.codefixer.update;
 public class UpdateManager {
 	
 	public static boolean versionIsNewest() {
-		if()
+		
 	}
 	
 	public static void openUpdateWindow() {
